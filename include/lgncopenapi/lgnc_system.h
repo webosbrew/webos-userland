af5466f9af4a2b46d71b672e24f224d7de05682a
Ningyuan Li <mariotaku.lee@gmail.com>
2023-06-19 19:12:33 +0900

    updated lgncopenapi

--- include/lgncopenapi/lgnc_system.h
+++ include/lgncopenapi/lgnc_system.h
@@ -76,7 +76,6 @@ enum LGNC_CURSOR_HOTSPOT_T {
 };
 
 
-
 typedef struct LGNC_CTRL_INFO LGNC_CTRL_INFO, *PLGNC_CTRL_INFO;
 
 struct LGNC_CTRL_INFO {
@@ -118,11 +117,15 @@ int LGNC_SYSTEM_Finalize(void);
 
 int LGNC_SYSTEM_GetDisplayId(int *displayId /* very likely to be wl_egl_window */);
 
-int LGNC_SYSTEM_GetLanguage(void);
+int LGNC_SYSTEM_GetDisplayResolution(int *width, int *height);
 
-int LGNC_SYSTEM_GetSaveDirectory(void);
+int LGNC_SYSTEM_GetLanguage(char *language);
 
-int LGNC_SYSTEM_GetSystemInfo(void);
+int LGNC_SYSTEM_GetSaveDirectory(char *directory);
+
+int LGNC_SYSTEM_GetSystemInfo(LGNC_CTRL_INFO *info);
+
+int LGNC_SYSTEM_GetVersion(int *major, int *minor, int *patch);
 
 int LGNC_SYSTEM_SetCursorShape(void);
 
